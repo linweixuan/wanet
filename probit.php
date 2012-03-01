610c80aa6a5396ee757a91e699eca7b4c77135e8
linweixuan <linweixuan@gmail.com>
2012-03-01 23:06:03 +0800

    add deny and search pages
    Signed-off-by: linweixuan <linweixuan@gmail.com>

---
+++ probit.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>广乎重工</title>
+<link rel="stylesheet" type="text/css" href="index.css">
+<link rel="stylesheet" type="text/css" href="script/tab/tabpanel.css">
+<script type="text/javascript" src="script/jquery-1.5.1.js"></script>
+<script type="text/javascript" src="script/jquery.json-2.3.js"></script>
+<script type="text/javascript" src="script/tab/fader.js"></script>
+<script type="text/javascript" src="script/tab/tabpanel.js"></script>
+<script type="text/javascript" src="script/tab/math.uuid.js"></script>
+<script type="text/javascript" src="script/wanet.tab.js"></script>
+<script type="text/javascript" src="script/wanet.ifram.js"></script>
+</head>
+<body>
+<?php require_once './lib/login.php'; ?>
+<div class="header_bg">
+  <div class="header">
+    <h1 class="logo"><a href="http://www.wanet.cn/" title="广福重工">广福重工</a></h1>
+    <span class="tel">全国统一服务热线：020-3223-7036</span>
+    <div class="login"> <a href="http://www.wanet.cn/register.html" target="_top">注册</a> 
+    <a href="http://www.wanet.cn/logout.html" target="_top">退出</a> </div>
+    <div class="links"> <a href="http://www.wanet.cn/help/index.html" target="_top" class="help">帮助</a>
+    <a href="http://www.wanet.cn/chain.html" target="_top" class="agency">连锁</a>
+    <a href="http://www.wanet.cn/gf.html" target="_top" class="vbokecc">广福</a> </div>
+  </div>
+</div>
+<div class="menu">
+  <ul>
+    <li><a href="http://www.wanet.cn/index.html">首页</a></li>
+    <li><a href="http://www.wanet.cn/index.html">基础资料</a></li>
+    <li><a href="http://www.wanet.cn/index.html">采购管理</a></li>
+    <li class="hover"><a href="http://www.wanet.cn/index.html">销售管理</a></li>
+    <li><a href="http://www.wanet.cn/index.html">库存管理</a></li>
+    <li><a href="http://www.wanet.cn/index.html">应收应付</a></li>
+    <li><a href="http://www.wanet.cn/index.html">现金银行</a></li>
+    <li><a href="http://www.wanet.cn/index.html">账务核算</a></li>
+    <li><a href="http://www.wanet.cn/index.html">系统管理</a></li>    
+  </ul>
+</div>
+<div class="content">
+  <div class="box_w">
+    <div class="box">
+    <iframe name="help" id="help" src="help/index.html" frameborder="1" scrolling="no" onLoad="autoFrameHeightId(help)" >
+    Your browser doesn't support iframes.</iframe>
+    </div>
+  </div>
+</div>
+
+<div class="footer_banner">
+  <div class="f_b_box"></div>
+</div>
+
+</body>
+</html>
